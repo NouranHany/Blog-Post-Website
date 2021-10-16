bda2263a4f99051dfcaab10c11514b57c5a7f707
NouranHany <hany.noran@yahoo.com>
2021-10-16 12:44:27 +0200

    Creates Main Routes

--- app.js
+++ app.js
@@ -15,19 +15,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// ----------------- Start Of website Routes -----------
+app.get("/",function(req,res){
+  res.render("home",{homeStartingContent: homeStartingContent});
+});
 
+app.get("/about",function(req,res){
+  res.render("about",{aboutContent:aboutContent})
+});
 
+app.get("/contact",function(req,res){
+  res.render("contact",{contactContent: contactContent});
+});
 
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
 
+app.post("/compose",function(req,res){
+  const post = {
+    postTitle: req.body.postTitle,
+    postBody: req.body.postBody
+  };
+  console.log(post);
+});
+// ----------------- End Of website Routes -----------
 
-
-
-
-
-
-
-
-
+// ----------------- Starting the Server -------------
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
